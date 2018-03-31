dab3ff6effdb470744ab9d8af1b4debdc3fafc37
Rali Ralev <rralev@icloud.com>
2018-03-31 15:13:41 +0300

    Optimistic locking on Accounts

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180331114151) do
+ActiveRecord::Schema.define(version: 20180331121046) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20180331114151) do
     t.string "currency_code", null: false
     t.decimal "balance", precision: 30, scale: 18, default: "0.0", null: false
     t.bigint "user_id"
+    t.integer "lock_version", default: 0, null: false
     t.index ["user_id"], name: "index_accounts_on_user_id"
   end
 
