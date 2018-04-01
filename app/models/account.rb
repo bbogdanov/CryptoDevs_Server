05f26c9ddd8ff88face021bcd0990b2580c0b2ec
Rali Ralev <rralev@icloud.com>
2018-04-01 12:08:00 +0300

    add addresses on user creation

--- app/models/account.rb
+++ app/models/account.rb
@@ -4,17 +4,25 @@ class Account < ApplicationRecord
   has_many :pending_withdrawals
 
   def self.create_btc_account(user)
-    acc = Account.new
-    acc.currency_code = 'BTC'
-    acc.user = user
-    acc.save!
+    ActiveRecord::Base.transaction do
+      acc = Account.new
+      acc.currency_code = 'BTC'
+      acc.user = user
+      addr = AddressPool.first_free('BTC')
+      acc.address = addr.address
+      acc.save!
+    end
   end
 
   def self.create_eth_account(user)
-    acc = Account.new
-    acc.currency_code = 'ETH'
-    acc.user = user
-    acc.save!
+    ActiveRecord::Base.transaction do
+      acc = Account.new
+      acc.currency_code = 'ETH'
+      acc.user = user
+      addr = AddressPool.first_free('ETH')
+      acc.address = addr.address
+      acc.save!
+    end
   end
 
   def deposit(amount)
--- app/models/address_pool.rb
+++ app/models/address_pool.rb
@@ -1,2 +1,8 @@
 class AddressPool < ApplicationRecord
+  def self.first_free(currency_code)
+    address = AddressPool.where(status: 'free').where(currency_code: currency_code).first
+    address.status = 'taken'
+    address.save!
+    address
+  end
 end
